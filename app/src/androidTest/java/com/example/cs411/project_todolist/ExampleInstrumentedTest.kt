430a649606c1b6837aa434fac9626c6b1c569da5
Omar Ramirez <47376992+oramirez1397@users.noreply.github.com>
2021-05-16 18:19:37 -0700

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/cs411/project_todolist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/cs411/project_todolist/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.vicky.todolist
+package com.example.cs411.project_todolist
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.vicky.todolist", appContext.packageName)
+        assertEquals("com.example.cs411.project_todolist", appContext.packageName)
     }
 }
